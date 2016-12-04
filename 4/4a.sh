1b4c393fe5ecae33e339ac679b65bce73b27f69f
Thomas ten Cate <ttencate@gmail.com>
2016-12-04 17:59:21 +0100

    Shorten solutions for 4

--- 4/4a.sh
+++ 4/4a.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-function checksum() {
-  grep -o '[a-z]' | sort | uniq -c | sort -rns | head -n5 | grep -o '[a-z]' | tr -d '\n'
-}
-
 sum=0
 while read line; do
   [[ $line =~ ^([a-z-]+)-([0-9]+)\[([a-z]+)\]$ ]]
   name=${BASH_REMATCH[1]}
   code=${BASH_REMATCH[2]}
   input_checksum=${BASH_REMATCH[3]}
-  computed_checksum=$(echo $name | checksum)
-  if [[ $input_checksum == $computed_checksum ]]; then
-    (( sum += code ))
-  fi
+  computed_checksum=$(echo $name | grep -o '[a-z]' | sort | uniq -c | sort -rns | head -n5 | grep -o '[a-z]' | tr -d '\n')
+  [[ $input_checksum == $computed_checksum ]] && (( sum += code ))
 done
 echo $sum
--- 4/4b.sh
+++ 4/4b.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-function decrypt() {
-  steps=$(($1 % 26))
-  alphabet=abcdefghijklmnopqrstuvwxyz
-  tr ${alphabet} ${alphabet:$steps}${alphabet:0:$((26 - steps))}
-}
-
+alphabet=abcdefghijklmnopqrstuvwxyz
 while read line; do
   [[ $line =~ ^([a-z-]+)-([0-9]+)\[([a-z]+)\]$ ]]
   name=${BASH_REMATCH[1]}
   code=${BASH_REMATCH[2]}
-  echo $(echo $name | decrypt $code)-$code
+  steps=$(($code % 26))
+  echo $(echo $name | tr ${alphabet} ${alphabet:$steps}${alphabet:0:$((26 - steps))})-$code
 done | grep north
