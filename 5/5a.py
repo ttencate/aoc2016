52ab01f70f3de3cf9061d754a7362f1b29d78b98
Thomas ten Cate <ttencate@gmail.com>
2016-12-05 08:39:18 +0100

    Solve 5a

---
+++ 5/5a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import hashlib
+import sys
+
+door_id = sys.stdin.readline().strip()
+index = 0
+password = ''
+
+while len(password) < 8:
+    hashed = '{0}{1}'.format(door_id, index)
+    md5 = hashlib.md5(hashed.encode('ascii')).hexdigest()
+    if md5.startswith(5 * '0'):
+        char = md5[5]
+        password += md5[5]
+        sys.stdout.write(char)
+        sys.stdout.flush()
+    index += 1
+
+sys.stdout.write('\n')
