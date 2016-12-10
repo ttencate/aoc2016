9c09d095c4bee463f4136a2543a89ca5c594aec6
Thomas ten Cate <ttencate@gmail.com>
2016-12-10 20:25:35 +0100

    Port 1a to COBOL

--- 01_cobol/01a.cob
+++ 01_cobol/01a.cob
@@ -1,5 +1,88 @@
        identification division.
-       program-id. one.
+       program-id. 01a.
+
+       environment division.
+       input-output section.
+       file-control.
+           select inputfile assign to keyboard
+           organization is line sequential.
+
+       data division.
+       file section.
+       fd inputfile is external
+           record is varying in size
+           data record is input-line.
+           01 input-line pic A(999).
+       working-storage section.
+           01 idx pic 999 value 001.
+           01 len pic 9.
+           01 lr pic A.
+           01 distance pic 999.
+           01 done pic 9 value 0.
+
+           01 x pic S999 value +000.
+           01 y pic S999 value +000.
+           01 total-distance pic 999.
+           01 direction pic 9 value 1.
+
        procedure division.
-           display 'hello world'.
+       main.
+           open input inputfile.
+           read inputfile.
+           close inputfile.
+
+           perform until done = 1
+             perform read-command
+             perform apply-command
+           end-perform
+
+           move function abs(x) to x.
+           move function abs(y) to y.
+           add x to y giving total-distance. 
+           display total-distance.
+
            stop run.
+
+       read-command.
+           move input-line(idx:1) to lr.
+           if lr <> "R" and lr <> "L" then
+             move 1 to done
+           end-if.
+           add 1 to idx.
+           move 0 to len.
+           perform until input-line(idx + len:1) = "," or
+                         input-line(idx + len:1) = " "
+             add 1 to len
+           end-perform.
+           move '   ' to distance.
+           move input-line(idx:len) to distance.
+           add len to idx.
+           add 2 to idx.
+
+       apply-command.
+           if lr = "R" then
+             add 1 to direction
+           else
+             subtract 1 from direction
+           end-if.
+           if direction = 5 then
+             move 1 to direction
+           end-if.
+           if direction = 0 then
+             move 4 to direction
+           end-if.
+
+           perform distance times
+             if direction = 1 then
+               add 1 to y
+             end-if
+             if direction = 2 then
+               add 1 to x
+             end-if
+             if direction = 3 then
+               subtract 1 from y
+             end-if
+             if direction = 4 then
+               subtract 1 from x
+             end-if
+           end-perform.
--- 01_cobol/README.md
+++ 01_cobol/README.md
@@ -8,4 +8,20 @@ SO, DAY ONE, IN THE COBOL LANGUAGE. THIS IS SO OLD THAT IT WAS PRETTY MUCH DEAD
 BEFORE I WAS BORN. I HAVE NO IDEA WHAT I'M DOING SO I INSTALLED GNUCOBOL. I
 FEEL LIKE A MASOCHIST ALREADY. TIMES WILL BE INTERESTING.
 
-Huh, okay. Actually GnuCOBOL is not case sensitive. Pleasant.
+Huh, okay. Actually GnuCOBOL is not case sensitive. I will try to enjoy this
+little bit of good fortune while it lasts.
+
+I'm using [this COBOL tutorial](https://www.tutorialspoint.com/cobol) to get a
+grip on this strange language. The [GNU Cobol Programmer's
+Guide](http://open-cobol.sourceforge.net/guides/GNU%20COBOL%202.0%20Programmers%20Guide.pdf)
+is also useful. In COBOL, indentation matters, but not in the way you might
+think: each column in the program plays a particular role. Vim's built-in COBOL
+mode (!) really helps here, by understanding what I'm typing and indenting it
+correctly.
+
+Simply want to declare an integer variable? Looks like this: `01 x pic S999.
+value +000.`. The `S999` bit means "a sign, followed by three digits"; this
+specifies how the variable will be laid out in memory. As a string, unless you
+write `usage is computational`, but why should I? This format, sadly, makes
+reading the variable-length integers in the input difficult, but I'm not going
+to cheat by formatting the input better.
