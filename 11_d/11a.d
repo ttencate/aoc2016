1df0fe9be96ab631f5d9007af67ea9a8c360b96c
Thomas ten Cate <ttencate@gmail.com>
2016-12-11 18:01:12 +0100

    Get 11a to work on the example input

--- 11_d/11a.d
+++ 11_d/11a.d
@@ -5,6 +5,8 @@ import std.array;
 import std.bitmanip;
 import std.container;
 import std.conv;
+import std.format;
+import std.range;
 import std.regex;
 import std.stdio;
 import std.typecons;
@@ -19,16 +21,92 @@ const uint GENERATOR_MASK = 0b01010101010101010101010101010101;
 const uint MICROCHIP_MASK = 0b10101010101010101010101010101010;
 
 alias Item = uint;
-alias ItemSet = uint;
 
 Item[string] elements;
 Item[] allItems;
 uint numElements;
 
+struct ItemSet {
+  uint mask;
+
+  void add(Item item) {
+    mask |= item;
+  }
+
+  void remove(Item item) {
+    mask &= ~item;
+  }
+
+  bool contains(Item item) const pure {
+    return !!(mask & item);
+  }
+
+  bool isSafe() const pure {
+    uint generators = mask & GENERATOR_MASK;
+    uint unpairedMicrochips = mask & MICROCHIP_MASK & ~(generators << 1);
+    return !generators || !unpairedMicrochips;
+  }
+
+  bool isFull() const {
+    return mask == (1 << (numElements * 2)) - 1;
+  }
+
+  int opApply(int delegate(Item item) dg) {
+    int result = 0;
+    foreach (ulong bit; iota(0, numElements * 2)) {
+      Item item = to!Item(1 << bit);
+      if (contains(item)) {
+        result = dg(item);
+        if (result) {
+          break;
+        }
+      }
+    }
+    return result;
+  }
+}
+
 struct State {
   ItemSet[NUM_FLOORS] floors;
   uint currentFloor;
-  uint stepsTaken;
+  State* prevState;
+
+  string toString() const {
+    char[] output;
+    foreach_reverse (floor; iota(0, NUM_FLOORS)) {
+      output ~= format("F%d %s  ", floor + 1, floor == currentFloor ? "E" : ".");
+      foreach (bit; iota(0, 2 * numElements)) {
+        auto item = to!Item(1 << bit);
+        if (floors[floor].contains(item)) {
+          output ~= format("%s%d ", item & MICROCHIP_MASK ? "M" : "G", bit / 2);
+        } else {
+          output ~= ".  ";
+        }
+      }
+      output ~= "\n";
+    }
+    return output.idup;
+  }
+
+  bool isSafe() const pure {
+    return floors[].all!(floor => floor.isSafe);
+  }
+
+  bool isEndState() const {
+    return floors[TOP_FLOOR].isFull;
+  }
+  
+  uint numSteps() const {
+    return prevState ? 1 + prevState.numSteps : 0;
+  }
+
+  State* dup() {
+    State* copy = new State();
+    copy.floors = floors;
+    copy.currentFloor = currentFloor;
+    copy.prevState = prevState;
+    return copy;
+  }
 }
 
 uint parseItem(char[] item) {
@@ -49,44 +127,31 @@ ItemSet parseFloor(char[] line) {
   assert(!match.empty);
   auto contents = match[1];
   if (contents == "nothing relevant") {
-    return 0;
+    return ItemSet();
   } else {
     ItemSet items;
     auto cre = ctRegex!(r"(, and |, | and )");
     foreach (item; contents.split(cre)) {
-      items |= parseItem(item);
+      items.add(parseItem(item));
     }
     return items;
   }
 }
 
-bool isEndState(State state) {
-  return state.floors[TOP_FLOOR] == (1 << (numElements * 2)) - 1;
+void moveItem(ref State state, Item item, uint from, uint to) {
+  state.floors[from].remove(item);
+  state.floors[to].add(item);
 }
 
-bool contains(ItemSet items, Item item) {
-  return (items & item) != 0;
-}
-
-bool isSafe(ItemSet items) {
-  uint generators = items & GENERATOR_MASK;
-  uint unpairedMicrochips = items & MICROCHIP_MASK & ~(generators << 1);
-  return !generators || !unpairedMicrochips;
-}
-
-bool isSafe(State state) {
-  return state.floors[].all!isSafe;
-}
-
-bool moveItem(ref State state, Item item, uint from, uint to) {
-  auto fromItems = state.floors[from] & ~item;
-  auto toItems = state.floors[to] | item;
-  if (!fromItems.isSafe || !toItems.isSafe) {
-    return false;
+void printWithHistory(State* endState) {
+  auto stack = Array!(State*)();
+  stack.insertBack(endState);
+  while (stack.back.prevState) {
+    stack.insertBack(stack.back.prevState);
   }
-  state.floors[from] = fromItems;
-  state.floors[to] = toItems;
-  return true;
+  foreach_reverse (state; stack) {
+    writeln(*state);
+  }
 }
 
 void main() {
@@ -96,38 +161,37 @@ void main() {
     .array();
   numElements = to!uint(elements.length);
 
-  auto startState = State(floors[0..NUM_FLOORS], BOTTOM_FLOOR, 0);
+  auto startState = new State(floors[0..NUM_FLOORS], BOTTOM_FLOOR, null);
 
-  auto queue = DList!State();
+  auto queue = DList!(State*)();
   queue.insertBack(startState);
   while (!queue.empty) {
     auto state = queue.front();
     queue.removeFront();
 
     if (state.isEndState()) {
-      writeln(state.stepsTaken);
-      break;
+      printWithHistory(state);
+      writeln(state.numSteps);
+      return;
     }
 
-    foreach (uint direction; [1, -1]) {
-      uint fromFloor = state.currentFloor;
-      uint toFloor = fromFloor + direction;
+    uint fromFloor = state.currentFloor;
+    foreach (uint toFloor; [fromFloor + 1, fromFloor - 1]) {
       if (toFloor < BOTTOM_FLOOR || toFloor > TOP_FLOOR) {
         continue;
       }
 
-      State baseNextState = state;
+      State baseNextState = *state;
+      baseNextState.prevState = state;
       baseNextState.currentFloor = toFloor;
-      baseNextState.stepsTaken++;
 
-      auto items = bitsSet(state.floors[state.currentFloor])
-        .map!(bit => to!Item(1 << bit));
+      auto items = state.floors[state.currentFloor];
 
       foreach (Item item; items) {
         State nextState = baseNextState;
         nextState.moveItem(item, fromFloor, toFloor);
         if (nextState.isSafe) {
-          queue.insertBack(nextState);
+          queue.insertBack(nextState.dup);
         }
       }
 
@@ -139,7 +203,7 @@ void main() {
           State nextState = baseNextState;
           nextState.moveItem(item1 | item2, fromFloor, toFloor);
           if (nextState.isSafe) {
-            queue.insertBack(nextState);
+            queue.insertBack(nextState.dup);
           }
         }
       }
